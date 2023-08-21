6163ab877f21cfdd919fb2a0b5bba9452b36e12a
flowerinthenight <flowerinthenight@pm.me>
2023-08-22 08:25:18 +0900

    chore: fmt

--- hasher.go
+++ hasher.go
@@ -4,6 +4,4 @@ import "github.com/cespare/xxhash"
 
 type hasher struct{}
 
-func (h hasher) Sum64(data []byte) uint64 {
-	return xxhash.Sum64(data)
-}
+func (h hasher) Sum64(data []byte) uint64 { return xxhash.Sum64(data) }
--- member.go
+++ member.go
@@ -2,6 +2,4 @@ package main
 
 type Member string
 
-func (m Member) String() string {
-	return string(m)
-}
+func (m Member) String() string { return string(m) }
