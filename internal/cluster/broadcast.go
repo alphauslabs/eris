8e6da8c004b8be335ef429bc891d5ca02fe33d66
flowerinthenight <flowerinthenight@pm.me>
2023-09-20 22:49:21 +0900

    chore(jupiter): fail for non-string reply

--- internal/cluster/broadcast.go
+++ internal/cluster/broadcast.go
@@ -108,7 +108,9 @@ func doDistributedGet(cd *ClusterData, e *cloudevents.Event) ([]byte, error) {
 	case []interface{}:
 		for i, d := range v.([]interface{}) {
 			if _, ok := d.(string); !ok {
-				mb[mgetIds[i]] = []byte("")
+				e := fmt.Errorf("unexpected non-string value for [%v:%v]", in.Name, mgetIds[i])
+				glog.Error(e)
+				return nil, e
 			} else {
 				mb[mgetIds[i]] = []byte(d.(string))
 			}
