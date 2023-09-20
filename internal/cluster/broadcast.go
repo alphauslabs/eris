929a96387c27540c297f9a5afcb52e3b7f9f8a9a
flowerinthenight <flowerinthenight@pm.me>
2023-09-21 01:34:20 +0900

    chore(jupiter): forgot hash key in distget, dbg

--- internal/cluster/broadcast.go
+++ internal/cluster/broadcast.go
@@ -109,8 +109,7 @@ func doDistributedGet(cd *ClusterData, e *cloudevents.Event) ([]byte, error) {
 	case []interface{}:
 		for i, d := range v.([]interface{}) {
 			if _, ok := d.(string); !ok {
-				e := fmt.Errorf("unexpected non-string value for [%v:%v], type=%T, val=%v",
-					in.Name, mgetIds[i], d, d)
+				e := fmt.Errorf("unexpected non-string value for %v, type=%T", in.Name, d)
 				glog.Error(e)
 				return nil, e
 			} else {
